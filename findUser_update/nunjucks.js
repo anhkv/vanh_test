5ef9bcfebd08738fdb3a7e4c8546eaf4ff67df18
kimvietanh <anhkv@sphinx-software.com>
2019-04-15 13:47:48 +0700

    viet anh commit folder findUser_update

---
+++ findUser_update/nunjucks.js
@@ -0,0 +1,17 @@
+
+const nunjucks = require('nunjucks');
+const Koa = require('koa');
+const Router = require('koa-router');
+const knex = require('./config');
+
+const app  = new Koa();
+var router = new Router();
+
+router.get('/vanh/:name',async (ctx,next) => {
+    let data = await knex.select('*').from('users').where('name', 'like', '%'+ctx.params.name+'%');
+    ctx.body = nunjucks.render('index.html',{data});
+});
+
+
+app.use(router.routes());
+app.listen(65535);
